ee1f8e2e6d53a93cba21e94720ed1abb34fdeca8
Zahra Aulia Nasution <zahraauliaw13@gmail.com>
2026-06-24 11:34:37 +0700

    fix: sesuaikan env variable session store

--- app.js
+++ app.js
@@ -27,10 +27,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Session configuration
 const sessionStore = new MySQLStore({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 app.use(session({
